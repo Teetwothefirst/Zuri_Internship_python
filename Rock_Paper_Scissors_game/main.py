76dc568999799b1d15fc5be203bca1b069a486f9
Teetwothefirst <dexterdavid835@gmail.com>
2022-06-09 10:17:13 +0100

    RPS game

---
+++ Rock_Paper_Scissors_game/main.py
@@ -0,0 +1,8 @@
+#Rock Paper Scissors game
+import random
+
+print("Welcome to this Rock Paper Scissors game")
+user_input = input("Player One enter your pick e.g Rock, Paper or Scissors: ")
+list_of_possible_choice = ["Rock", "Paper", "Scissors"]
+
+print(list_of_possible_choice[0])
