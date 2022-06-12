897d2edf7bba659d72d6b05485b42eb19c0b5302
Teetwothefirst <dexterdavid835@gmail.com>
2022-06-12 23:15:28 +0100

    New commit

--- Rock_Paper_Scissors_game/main.py
+++ Rock_Paper_Scissors_game/main.py
@@ -1,4 +1,5 @@
 #Rock Paper Scissors game
+
 import random
 from secrets import choice
 
@@ -10,19 +11,27 @@ player_input = input("Player One enter your pick e.g Rock, Paper or Scissors: ")
 computer_choice = choice(list_of_possible_choice)
 print("Player 1 chose "+ player_input)
 print("CPU chose "+computer_choice)
-if player_input == "ROCK" or player_input == "Rock" or player_input == "rock":
-    player_input = "ROCK"
-if player_input == "PAPER" or player_input == "Paper" or player_input == "paper":
-    player_input = "PAPER" 
-if player_input == "SCISSORS" or player_input == "Scissors" or player_input == "scissors":
-    player_input = "SCISSORS"
+
+
+option1 = list_of_possible_choice[0]
+option2 = list_of_possible_choice[1]
+option3 = list_of_possible_choice[2]
+
 
 for i in list_of_possible_choice:
-    print (i) 
-    print (i[0])
-    print (i[1])
-    print (i[2])
-    if player_input != i:
-        print("The option you chose is invalid, try again")
-    
-print(list_of_possible_choice[0])
+    print(i)
+      
+    #All the possible choice the player could Enter to reduce more errors.  
+    if player_input == "ROCK" or player_input == "Rock" or player_input == "rock" or player_input == "R" or player_input == "r":
+        player_input = "ROCK" 
+        option1 == player_input
+    if player_input == "PAPER" or player_input == "Paper" or player_input == "paper" or player_input =="P" or player_input == "p":
+        player_input = "PAPER" 
+        option2 == player_input
+    if player_input == "SCISSORS" or player_input == "Scissors" or player_input == "scissors" or player_input == "S" or player_input =="s":
+        player_input = "SCISSORS"
+        option3 == player_input
+    else: 
+        print("The option you chose is invalid")
+    # if player_input != option1 or player_input != option2 or player_input != option3:
+    #         print("The option you chose isn't part of the possible options, Try Again" )
